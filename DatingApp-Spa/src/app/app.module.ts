c2aba8fee83622c8466a84c9e9087b2654736a2f
kidalv1 <vladik1719@hotmail.com>
2020-06-08 17:54:02 +0200

    creat

---
+++ DatingApp-Spa/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { ValueComponent } from './value/Value.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      ValueComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      HttpClientModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule { }
